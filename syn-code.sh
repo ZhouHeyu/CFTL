9f38f5f38b86bba978a831cabee88be1201150b0
yanmingbo <w547108@126.com>
2018-06-25 14:44:33 +0800

    导入自动运行脚本

---
+++ syn-code.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 首先注意清理
+make clean
+# 从github上同步代码
+echo "从github 上同步代码,lab206的git密码为:123456"
+git pull origin master
+#切换目录到上级目录make
+cd ../ ; make
+read -p "if make is successed yes?no" flag
+
+if [ $flag == "yes" ]; then
+	cd test.release ; source runtest
+	# 切换回原来目录
+	cd ../src
+else
+	# 切换回原来目录
+	cd src
+fi
+
+
