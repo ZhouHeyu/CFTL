5b1ea5d2afb3765c12d86aa7798a41f3fe61f0ec
=zhoujie <=161080017@hdu.edu.cn>
2018-06-30 00:24:13 +0800

    debug CPFTL

--- ssd_interface.c
+++ ssd_interface.c
@@ -1983,12 +1983,13 @@ void C_CMT_Is_Full()
       for(indexold = 0;indexold < MAP_SECOND_MAX_ENTRIES; indexold++){
           if(((second_arr[indexold]-MLC_page_num_for_2nd_map_table)/MLC_MAP_ENTRIES_PER_PAGE) == maxentry){
             //test debug
+            curr_lpn=second_arr[indexold];
             printf("curr_lpn is %d\tindex_old is %d\n 
                     MLC_page_num for_2nd_map_table is %d\tMLC_MAP_ENTRIES_PER_PAGE is %d\n
                     maxentry is %d\n",
                       curr_lpn,indexold,MLC_page_num_for_2nd_map_table,MLC_MAP_ENTRIES_PER_PAGE,maxentry);
 
-            curr_lpn=second_arr[indexold];
+            
             MLC_opagemap[curr_lpn].map_status=MAP_INVALID;
             MLC_opagemap[curr_lpn].map_age=0;
             MLC_opagemap[curr_lpn].update=0;
