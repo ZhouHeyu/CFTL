9dac71c4b3d4cdcc7a2965c536ed66bd6f464a56
=zhoujie <=161080017@hdu.edu.cn>
2018-06-30 13:03:27 +0800

    删除CCMT_is_full的debug代码

--- ssd_interface.c
+++ ssd_interface.c
@@ -1972,19 +1972,6 @@ void C_CMT_Is_Full()
       send_flash_request(maxentry*8,8,0,2,1);
       translation_write_num++;
 
-			//debug test
-		for(i=0;i<MLC_MAP_ENTRIES_PER_PAGE;i++){
-			offset=i+MLC_page_num_for_2nd_map_table+maxentry*MLC_MAP_ENTRIES_PER_PAGE;
-			if(MLC_opagemap[offset].map_status==MAP_SECOND){
-				if(search_table(second_arr,MAP_SECOND_MAX_ENTRIES,offset)==-1){
-					printf("before CCMT delete is failded\n");
-					assert(0);
-				}
-			}
-		}
-	
-
-      
       //sencond_arr数组里面存的是lpn,将翻译页关联的映射项全部置为无效
       for(indexold = 0;indexold < MAP_SECOND_MAX_ENTRIES; indexold++){
           if(((second_arr[indexold]-MLC_page_num_for_2nd_map_table)/MLC_MAP_ENTRIES_PER_PAGE) == maxentry){
@@ -1996,19 +1983,6 @@ void C_CMT_Is_Full()
           }
 		}
 
-		
-		//debug test
-		for(i=0;i<MLC_MAP_ENTRIES_PER_PAGE;i++){
-			offset=i+MLC_page_num_for_2nd_map_table+maxentry*MLC_MAP_ENTRIES_PER_PAGE;
-			if(MLC_opagemap[offset].map_status==MAP_SECOND){
-				if(search_table(second_arr,MAP_SECOND_MAX_ENTRIES,offset)==-1){
-					printf("after CCMT delete is failded\n");
-					assert(0);
-				}
-			}
-		}
-
-        
         
     }
 }
